b2e8cf05415ee2bd523dcd3923bed12351295e60
unknown <gs.coelho@outlook.com>
2021-03-29 15:36:59 -0300

    Uso da função sorted

---
+++ script_Function_Sorted.py
@@ -0,0 +1,11 @@
+valores = input().split(' ')
+a = int(valores[0])
+b = int(valores[1])
+c = int(valores[2])
+lista = (a, b, c)
+x = sorted(lista)
+
+v1 = x[0]
+v2 = x[1]
+v3 = x[2]
+print(v1, v2, v3)
